cbb98c7799ec418bc1fcd8a69f8983278b42b566
Michael Cipold <github@cipold.de>
2017-09-25 17:06:41 +0200

    allow module installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+	name='periodicx',
+	version='1.0',
+	description='This module exports a single function which allows the user to call a function periodically with high precision and absolute timing.',
+	author='Michael Cipold',
+	author_email='github@cipold.de',
+	url='https://github.com/cipold/periodicx',
+	py_modules=['periodicx'],
+	license='MPL-2.0',
+)
